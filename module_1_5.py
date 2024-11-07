3a73870ccf23a55318ce919661a21785f210276a
Rodion792 <duninrodion@gmail.com>
2024-11-07 16:35:22 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 2, 3, 4, 'a', 'b', 'c')
+print(immutable_var)
+# immutable_var[1] = 5 данная строка не будет выполненна так как в кортеже нельзя изменять данные
+mutable_list = [1, 2, 3 , 'a', 'b', 'Urban']
+mutable_list[3] = 663
+mutable_list[2] = 8463
+print(mutable_list)
